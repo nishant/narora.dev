3016b42ac7ce9640179a8412f9e26bf60c128603
Nishant Arora <nish54@gmail.com>
2020-08-15 03:37:01 -0500

    social media bar done

--- src/app/components/social-media-bar/social-media-bar.component.ts
+++ src/app/components/social-media-bar/social-media-bar.component.ts
@@ -6,6 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./social-media-bar.component.scss']
 })
 export class SocialMediaBarComponent implements OnInit {
+  items = {
+    Github: {
+      url: 'http://github.com/nishant',
+      icon: 'fa-github',
+      background: '#24282E'
+    },
+    YouTube: {
+      url: 'https://www.youtube.com/channel/UCPaaYIhtC20QUtZfDW83ITA/',
+      icon: 'fa-youtube',
+      background: '#FF0100'
+    },
+    Instagram: {
+      url: 'https://www.instagram.com/narora101/',
+      icon: 'fa-instagram',
+      background: '#DA2B96'
+    },
+    LinkedIn: {
+      url: 'https://www.linkedin.com/in/nishant-arora-umd/',
+      icon: 'fa-linkedin',
+      background: '#0077B5'
+    }
+  };
 
   constructor() { }
 
